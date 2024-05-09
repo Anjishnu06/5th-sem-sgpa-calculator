d9b47e0709d61ac9b4a504f8ee8ebb3d0ee03111
Dora Explorer <doratheexplorer20241@gmail.com>
2024-05-09 21:27:04 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Calculator from './components/Calculator';
 function App() {
   return (
     <div className="container mt-5 px-3">
-        <h3 style={{paddingLeft:"300px"}}>5TH SEM SGPA CALCULATOR</h3>
+        <h3>5TH SEM SGPA CALCULATOR</h3>
         <Calculator/>
         <footer style={{paddingLeft:"300px"}}>
           Made with &hearts; by Anjishnu Kumar
